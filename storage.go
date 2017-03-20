c70a562ba663a54da185057ab14b99e103a8502f
xupeilin <xpeilin@gmail.com>
2017-03-20 21:05:27 +0800

    add interface in boltdb adapter

--- storage.go
+++ storage.go
@@ -10,16 +10,16 @@ const configBucket string = "config"
 const statBucket string = "stat"
 
 type DB struct {
-	bolt *bolt.DB
+	boltdb *bolt.DB
 }
 
 func OpenDB(path string) (db *DB, err error) {
-	bolt, e := bolt.Open("path", 0600, nil)
+	boltdb, e := bolt.Open("path", 0600, nil)
 	if e != nil {
 		fmt.Fprintf(os.Stderr, "Open database error: %s.", e)
 		return nil, e
 	}
-	err = bolt.Update(func(tx *bolt.Tx) error {
+	err = boltdb.Update(func(tx *bolt.Tx) error {
 		b, err := tx.CreateBucketIfNotExists([]byte(configBucket))
 		if err != nil {
 			return fmt.Errorf("create bucket(%s): %s", configBucket, err)
@@ -30,19 +30,33 @@ func OpenDB(path string) (db *DB, err error) {
 		}
 		return nil
 	})
-	db = &DB{bolt: bolt}
+	db = &DB{boltdb: boltdb}
 	return db, nil
 }
 
 func (p *DB) Close() error {
-	e := p.Close()
+	e := p.boltdb.Close()
 	if e != nil {
 		fmt.Fprintf(os.Stderr, "Close database error: %s.", e)
 	}
 	return e
 }
 
-func saveKeyValue(bucket string, key string, value string) bool {
+func (p *DB) put(bucket string, key string, value string) error {
+	err := p.boltdb.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		err := b.Put([]byte(key), []byte(value))
+		return err
+	})
+	return err
+}
 
-	return true
+func (p *DB) get(bucket string, key string) (string, error) {
+	var value []byte
+	err := p.boltdb.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		value := b.Get([]byte(key))
+		return err
+	})
+	return string(value[:]), err
 }
