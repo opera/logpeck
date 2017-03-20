b91ce0c5e1515b30a4a3fb929733d11e8ea6d008
xupeilin <xpeilin@gmail.com>
2017-03-20 15:57:43 +0800

    add http protocol

---
+++ protocol.go
@@ -0,0 +1,38 @@
+package logpeck
+
+type ESConfig struct {
+	URL   string
+	Index string
+	Type  string
+}
+
+type PeckTaskConfig struct {
+	Name       string
+	LogPath    string
+	FilterExpr string
+	ESConfig   EsConfig
+}
+
+type Stat struct {
+	LinesPerSec int64
+	BytesPerSec int64
+	LinesTotal  int64
+	BytesTotal  int64
+}
+
+type PeckTaskStat struct {
+	Name string
+	Stat Stat
+}
+
+type LogStat struct {
+	LogPath         string
+	PeckTaskConfigs []PeckTaskConfig
+	PeckTaskStats   []PeckTaskStat
+}
+
+type PeckerStat struct {
+	Name     string
+	Stat     string
+	LogStats []LogStat
+}
