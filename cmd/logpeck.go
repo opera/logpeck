1daba85ccb46eaa6d67b0cb4c0e027f9de54871c
xupeilin <xpeilin@gmail.com>
2017-03-23 16:27:50 +0800

    add http server in logpeck main

--- cmd/logpeck.go
+++ cmd/logpeck.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/go-zoo/bone"
 	"github.com/opera/logpeck"
+	"net/http"
+	"time"
 )
 
 func main() {
-	fmt.Println("hello logpeck")
+	configFile := flag.String("config", "./logpeck.conf", "Config file path")
+	flag.Parse()
+
+	logpeck.InitConfig(configFile)
+
 	pecker, err := logpeck.NewPecker()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(pecker)
+
+	mux := bone.New()
+	//	mux.Get("/pecker_stat", http.HandlerFunc(handler.Get))
+	//	mux.Post("/peck_task/add", http.HandlerFunc(handler.NewsRedirectHandler))
+	//	mux.Post("/peck_task/remove", http.HandlerFunc(handler.NewsRedirectHandler))
+
+	address := fmt.Sprintf(":%d", logpeck.Config.Port)
+
+	fmt.Printf("Logpeck start serving on %s .\n", address)
+	s := &http.Server{
+		Addr:         address,
+		Handler:      mux,
+		ReadTimeout:  2 * time.Second,
+		WriteTimeout: 2 * time.Second,
+	}
+	s.ListenAndServe()
 }
--- config.go
+++ config.go
@@ -6,21 +6,21 @@ import (
 	"os"
 )
 
-type PeckerConfig struct {
-	Port       int32     `toml:"port"`
-	MaxTaskNum int32     `toml:"max_task_num"`
-	TaskLimit  TaskLimit `toml:"task_limit"`
+type LogPeckConfig struct {
+	Port          int32         `toml:"port"`
+	MaxTaskNum    int32         `toml:"max_task_num"`
+	PeckTaskLimit PeckTaskLimit `toml:"peck_task_limit"`
 }
 
-type TaskLimit struct {
+type PeckTaskLimit struct {
 	MaxLinesPerSec int64 `toml:"max_lines_per_sec"`
 	MaxBytesPerSec int64 `toml:"max_bytes_per_sec"`
 }
 
-var PkConfig PeckerConfig
+var Config LogPeckConfig
 
-func InitConfig(file string) bool {
-	if _, err := toml.DecodeFile(file, &PkConfig); err != nil {
+func InitConfig(file *string) bool {
+	if _, err := toml.DecodeFile(*file, &Config); err != nil {
 		fmt.Fprintf(os.Stderr, "Parse config fail: %s.\n", err)
 		return false
 	}
