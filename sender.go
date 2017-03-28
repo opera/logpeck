d41368fa60231e3953b13963e4e012ddc1ec9a02
xupeilin <xpeilin@gmail.com>
2017-03-28 16:10:43 +0800

    add sender&unittest

---
+++ sender.go
@@ -0,0 +1,42 @@
+package logpeck
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type ElasticSearchSender struct {
+	url string
+}
+
+func NewElasticSearchSender(url string) {
+
+}
+
+type ElasticSearchData struct {
+	Host string
+	Log  string
+}
+
+func SendToElasticSearch(url, index, ty, content string) {
+	data := ElasticSearchData{
+		Host: GetHost(),
+		Log:  content,
+	}
+	log.Println(data)
+	raw_data, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	uri := "http://" + url + "/" + index + "/" + ty
+	log.Printf("Post ElasticSearch %s content [%s] ", url, raw_data)
+	body := ioutil.NopCloser(bytes.NewBuffer(raw_data))
+	resp, err := http.Post(uri, "application/json", body)
+	if err != nil {
+		log.Printf("Post error, err[%s], resp[%s]", err, resp)
+	}
+	log.Printf("Response %s err %s", resp, err)
+}
---
+++ sender_test.go
@@ -0,0 +1,13 @@
+package logpeck
+
+import (
+	"testing"
+)
+
+func TestSendToElasticSearch(*testing.T) {
+	url := "127.0.0.1:9200"
+	index := "test"
+	ty := "helloes"
+	content := "well done"
+	SendToElasticSearch(url, index, ty, content)
+}
