401fa0c381fe7d2c7484c765b124bda7b14b06eb
xupeilin <xpeilin@gmail.com>
2017-03-28 17:09:25 +0800

    add logmocker for test

---
+++ cmd/util/logmocker.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/opera/logpeck"
+)
+
+func Usage() {
+	s := "Usage: \n\t./logmocker --filename=<logfile>"
+	fmt.Println(s)
+}
+func main() {
+	filename := flag.String("filename", "", "log file name")
+	if *filename == "" {
+		Usage()
+		return
+	}
+	mocker, err := logpeck.NewMockLog(*filename)
+	if err != nil {
+		panic(err)
+	}
+	mocker.Run()
+}
