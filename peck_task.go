e4334bd43b3e828afd1fcf8627c8fe702d43e160
xupeilin <xpeilin@gmail.com>
2017-04-18 14:57:29 +0800

    PeckTask support multiple fields&type

--- peck_task.go
+++ peck_task.go
@@ -2,6 +2,7 @@ package logpeck
 
 import (
 	"log"
+	"strconv"
 )
 
 type PeckTask struct {
@@ -53,6 +54,28 @@ func (p *PeckTask) IsStop() bool {
 	return p.Stat.Stop
 }
 
+func (p *PeckTask) ExtractFields(content string) map[string]string {
+	if len(p.Config.Fields) == 0 {
+		return map[string]string{"Log": content}
+	}
+	fields := make(map[string]string)
+	arr := SplitString(content, p.Config.Delimiters)
+	for _, field := range p.Config.Fields {
+		if field.Value[0] != '$' {
+			panic(field)
+		}
+		pos, err := strconv.Atoi(field.Value[1:])
+		if err != nil {
+			panic(field)
+		}
+		if len(arr) < pos {
+			continue
+		}
+		fields[field.Name] = arr[pos-1]
+	}
+	return fields
+}
+
 func (p *PeckTask) Process(content string) {
 	if p.Stat.Stop {
 		return
@@ -60,6 +83,6 @@ func (p *PeckTask) Process(content string) {
 	if p.filter.Drop(content) {
 		return
 	}
-	fields := map[string]string{"Log": content}
+	fields := p.ExtractFields(content)
 	SendToElasticSearch(&p.Config.ESConfig, fields)
 }
--- sender_test.go
+++ sender_test.go
@@ -5,12 +5,12 @@ import (
 )
 
 func TestSendToElasticSearch(*testing.T) {
-	config := &ElasticSearchConfig{
-		URL:   "http://127.0.0.1:9200",
-		Index: "testes",
-		Type:  "log",
-	}
-	InitElasticSearchMapping(config)
+	//config := &ElasticSearchConfig{
+	//	URL:   "http://127.0.0.1:9200",
+	//	Index: "testes",
+	//	Type:  "log",
+	//}
+	//InitElasticSearchMapping(config)
 	//	panic(time.Now().Format("2006-01-02 15:04:05"))
 	//url := "127.0.0.1:9200"
 	//index := "test"
