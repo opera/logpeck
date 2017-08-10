05b5f3a1740c6e29514d1e09482baca788143072
xupeilin <xpeilin@gmail.com>
2017-08-10 15:26:03 +0800

    remove useless unit test

--- sender_test.go
+++ sender_test.go
@@ -1,7 +1,6 @@
 package logpeck
 
 import (
-	"encoding/json"
 	"fmt"
 	"testing"
 )
@@ -33,37 +32,4 @@ func TestGetIndexName(*testing.T) {
 			panic(indexName)
 		}
 	}
-}
-
-func TestMappingUnmarshal(*testing.T) {
-	raw := `{
-		"Hosts":["127.0.0.1:9200","127.0.0.1:9201"],
-		"Index":"mocklog10",
-		"Type":"Mocks",
-		"Mapping": \{
-			\"my_type\": \"properties\"
-		\}
-	}`
-	///"my_type/": /"properties/"
-	var config ElasticSearchConfig
-	err := json.Unmarshal([]byte(raw), &config)
-	if err != nil {
-		panic(err)
-	}
-	panic(config.Mapping)
-}
-
-func TestSendToElasticSearch(*testing.T) {
-	//config := &ElasticSearchConfig{
-	//	URL:   "http://127.0.0.1:9200",
-	//	Index: "testes",
-	//	Type:  "log",
-	//}
-	//InitElasticSearchMapping(config)
-	//	panic(time.Now().Format("2006-01-02 15:04:05"))
-	//url := "127.0.0.1:9200"
-	//index := "test"
-	//ty := "helloes"
-	//content := "well done"
-	//	SendToElasticSearch(url, index, ty, content)
 }
