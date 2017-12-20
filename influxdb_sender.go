3beab246c02819860e9023de96bf128074e0662a
xuanyuz <zxy_helloworld@163.com>
2017-12-20 10:12:50 +0800

    Add post in Send()

--- influxdb_sender.go
+++ influxdb_sender.go
@@ -1,8 +1,15 @@
 package logpeck
 
 import (
+	"bytes"
+	"fmt"
 	log "github.com/Sirupsen/logrus"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/httputil"
 	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -22,6 +29,7 @@ type InfluxDbConfig struct {
 	Hosts       string                      `json:"hosts"`
 	Interval    int64                       `json:"interval"`
 	Name        string                      `json:"name"`
+	DbName      string                      `json:"dbName"`
 	Aggregators map[string]AggregatorConfig `json:"aggregators"`
 }
 
@@ -44,12 +52,21 @@ func NewInfluxDbSender(senderConfig *SenderConfig, fields []PeckField) *InfluxDb
 func toInfluxdbLine(fields map[string]interface{}) string {
 	lines := ""
 	timestamp := fields["timestamp"].(int64)
+	host := ""
+	conn, err := net.Dial("udp", "google.com:80")
+	if err != nil {
+		fmt.Println(err.Error())
+		//return
+	}
+	defer conn.Close()
+	host = strings.Split(conn.LocalAddr().String(), ":")[0]
+
 	for k, v := range fields {
 		if k == "timestamp" {
 			continue
 		}
 		aggregationResults := v.(map[string]int64)
-		lines = k + " "
+		lines = k + ",host=" + host + " "
 		for aggregation, result := range aggregationResults {
 			lines += aggregation + "=" + strconv.FormatInt(result, 10) + ","
 
@@ -63,5 +80,15 @@ func toInfluxdbLine(fields map[string]interface{}) string {
 func (p *InfluxDbSender) Send(fields map[string]interface{}) {
 	lines := toInfluxdbLine(fields)
 	log.Infof("%s", lines)
+	raw_data := []byte(lines)
+	body := ioutil.NopCloser(bytes.NewBuffer(raw_data))
+	uri := "http://" + p.config.Hosts + "/write?db=" + p.config.DbName
+	resp, err := http.Post(uri, "application/json", body)
+	if err != nil {
+		log.Infof("[InfluxDbSender.Sender] Post error, err[%s]", err)
+	} else {
+		resp_str, _ := httputil.DumpResponse(resp, true)
+		log.Debugf("[InfluxDbSender.Sender] Response %s", resp_str)
+	}
 	//p.measurments.MeasurmentRecall(fields)
 }
